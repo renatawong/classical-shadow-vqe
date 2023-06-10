fee5d1f5c8b96895e33af740d7079de7d1f4bd4f
Renata Wong <renata.wong.kei@gmail.com>
2023-06-10 13:51:00 +0800

    Add files via upload

--- modified_derandomization.py
+++ modified_derandomization.py
@@ -37,7 +37,7 @@ def modified_derandomized_classical_shadow(all_observables, num_operators, syste
         cost = 0
         for i, zipitem in enumerate(zip(num_of_measurements_so_far, num_of_matches_needed_in_this_round)):
             measurement_so_far, matches_needed = zipitem
-            if num_of_measurements_so_far[i] >= math.floor(weight[i] * num_operators):  # changed
+            if num_of_measurements_so_far[i] >= math.floor(weight[i] * num_operators / len(all_observables)):  # changed
                 continue
 
             if system_size < matches_needed:
@@ -119,7 +119,7 @@ def modified_derandomized_classical_shadow(all_observables, num_operators, syste
 
         success = 0
         for i, single_observable in enumerate(all_observables):
-            if num_of_measurements_so_far[i] >= math.floor(weight[i] * num_operators):   # changed
+            if num_of_measurements_so_far[i] >= math.floor(weight[i] * num_operators / len(all_observables)):   # changed
                 success += 1
 
         if success == len(all_observables) and len(measurement_procedure) == num_operators:   # added second condition
